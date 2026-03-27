91c3eb6a90da31b02741f289a03abb219a81e1b6
Ryan Schuhler <ryan.schuhler@liferay.com>
2026-03-27 13:20:23 -0700

    Improvements

---
+++ src/i18n/ui.ts
@@ -0,0 +1,52 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  pt: 'Português',
+};
+
+export const domains = {
+  en: 'biblestudyleader.com',
+  es: 'liderdeestudiobiblico.com',
+  pt: 'líderdeestudosbíblicos.com',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.devotionals': 'Devotionals',
+    'nav.about': 'About',
+    'footer.rights': 'All rights reserved.',
+    'signup.title': 'Join the Mailing List',
+    'signup.error': 'Your subscription could not be saved. Please try again.',
+    'signup.success': 'Your subscription has been successful.',
+    'signup.placeholder': 'EMAIL',
+    'signup.button': 'Sign Up',
+    'signup.form_action': 'https://b7a11f55.sibforms.com/serve/MUIFAJFz8Wp6CXp7G7gG6e7zGjk51-7YtGG4pwh-Tj__RWkmGOsmgvviL_egXMXF2wOS48SST2EEbyaa1pnIp7JEMz0rCP6eQYKpT7XtyebeR68MQDNc8f5JBCcpbfi_yjfl6LVo7iTlQm2g6cuuwNQY4tI4MI79irjApPiyuOKDiVWfN3QYCEkLrdEVum1eNnByoL9ai_F4Sklq',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.devotionals': 'Devocionales',
+    'nav.about': 'Acerca De',
+    'footer.rights': 'Todos los derechos reservados.',
+    'signup.title': 'Únete a la lista de correo',
+    'signup.error': 'Su suscripción no se pudo guardar. Inténtalo de nuevo.',
+    'signup.success': 'Tu suscripción ha sido exitosa.',
+    'signup.placeholder': 'Correo electrónico',
+    'signup.button': 'Suscribirse',
+    'signup.form_action': 'https://b7a11f55.sibforms.com/serve/MUIFAP1yA_tz5ekQ0Amjkc7-XxS3-AWfWvx7QULkYdeg8MMajDDIDxk7X6SeouJkmcFBK4AXjQnHD79heXF1rmRGigk_37XybzDql2ertIFf4pVUob4VPUD6xTCnrhG04tckkjX4jUJEvpZNhP0ZoqsTjXGhdbQR0ewJ2xxnTFK96zDL6sEnEJjuaGnXnvTGhlX5sXAej9DXkLbK',
+  },
+  pt: {
+    'nav.home': 'Página Inicial',
+    'nav.devotionals': 'Devocionais',
+    'nav.about': 'Sobre',
+    'footer.rights': 'Todos os direitos reservados.',
+    'signup.title': 'Inscreva-se em nossa mailing list',
+    'signup.error': 'Sua inscrição não pode ser efetivada. Por favor, tente novamente.',
+    'signup.success': 'Inscrição efetivada com sucesso!',
+    'signup.placeholder': 'E-mail',
+    'signup.button': 'Inscrever-se',
+    'signup.form_action': 'https://b7a11f55.sibforms.com/serve/MUIFACgm349KKtuSTKdAQRadrkXIDslwMb7OeaGxfY1xyaZyMU9Ib6wBCDYfu5MCft4xayuLWvV79TJFTQQUVMAhJEgc-y3B2LC8C-Q5ljq6OpJ9JLgFm2FFAM5enEd_VOnCxzFxelpjpaS64fOiWiLgD7D7aKT5NSlGIFP4z_YgW5cdNp3ss_cxHe_3Meak_S7ZyYNs5M38fUXg',
+  },
+} as const;
